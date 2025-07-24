0bcfa46557f93d97bc82a2efe03731d51c27e199
Bruno Erick <141072611+bericktk@users.noreply.github.com>
2025-07-24 10:16:17 -0300

    Update chave-api.js

--- src/js/chave-api.js
+++ src/js/chave-api.js
@@ -1,5 +1,5 @@
 export const chaveApi = "AIzaSyCgoT5SPw4WrbGnA12AFBY6amkxvYtZ8yY";
 
-export const idPlaylist = "PLyDbNGjJEW2v0HjiG2QnjGa558m4PT4wO";
+export const idPlaylist = "PLvNOA3UHWY1-BYdtkSfXikuFNjyWYsG2S";
 
 export const idChannel = "UC3jJfCUy4LfMmuIavIsn-PA";
