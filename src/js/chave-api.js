5115a52c2ad9ac64f50aa22c6fffc2063555766f
bericktk <br.ericktk@gmail.com>
2025-07-31 21:03:07 -0300

    Remove arquivo chave-api.js e suas exportações de chave e IDs

--- src/js/chave-api.js
+++
@@ -1,5 +0,0 @@
-export const chaveApi = "AIzaSyCgoT5SPw4WrbGnA12AFBY6amkxvYtZ8yY";
-
-export const idPlaylist = "PLvNOA3UHWY1-BYdtkSfXikuFNjyWYsG2S";
-
-export const idChannel = "UC3jJfCUy4LfMmuIavIsn-PA";
